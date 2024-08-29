dfda01d1621d149a1810e7e04da261faa56fbc86
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-08-29 19:12:26 +0900

    👍 購入状況表示をビジュアル的に変更

--- public/lib/sessionChecker.js
+++ public/lib/sessionChecker.js
@@ -1,29 +1,29 @@
-const current_user_login = localStorage.getItem("current_user");
-if (!current_user_login) {
-  if (new URL(window.location.href).pathname !== '/welcome') {
-    window.location.href = "/welcome";
-  }
-}else{
-  const mydata = JSON.parse(current_user_login);
-  QuestTime(mydata);
-}
+// const current_user_login = localStorage.getItem("current_user");
+// if (!current_user_login) {
+//   if (new URL(window.location.href).pathname !== '/welcome') {
+//     window.location.href = "/welcome";
+//   }
+// }else{
+//   const mydata = JSON.parse(current_user_login);
+//   QuestTime(mydata);
+// }
 
-async function QuestTime(mydata){
-  if(mydata.tutorialEnded === false && new URL(window.location.href).pathname !== "/submit_post" && new URL(window.location.href).pathname !== '/quest' ){
-    window.location.href = "/quest";
-  }
-  //クエストの間隔を調整できます。../quest_completed.js Line22も確認
-  if((new Date(await GetNowTime()) - new Date(mydata.quest_completed_time)) >= 300000){//300000ミリ秒は五分
-    if (new URL(window.location.href).pathname !== '/quest' && new URL(window.location.href).pathname !== "/submit_post") {
-      window.location.href = "/quest";
-    }
-  }
-}
+// async function QuestTime(mydata){
+//   if(mydata.tutorialEnded === false && new URL(window.location.href).pathname !== "/submit_post" && new URL(window.location.href).pathname !== '/quest' ){
+//     window.location.href = "/quest";
+//   }
+//   //クエストの間隔を調整できます。../quest_completed.js Line22も確認
+//   if((new Date(await GetNowTime()) - new Date(mydata.quest_completed_time)) >= 300000){//300000ミリ秒は五分
+//     if (new URL(window.location.href).pathname !== '/quest' && new URL(window.location.href).pathname !== "/submit_post") {
+//       window.location.href = "/quest";
+//     }
+//   }
+// }
 
-async function GetNowTime() {
-  const result = await fetch("/time", {
-    method: "GET",
-  });
-  const nowTime = await result.text();
-  return nowTime;
-}
+// async function GetNowTime() {
+//   const result = await fetch("/time", {
+//     method: "GET",
+//   });
+//   const nowTime = await result.text();
+//   return nowTime;
+// }
