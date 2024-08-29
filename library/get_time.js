613036563795349ad80f61eb4ace65d3c5a84559
usatan6012 <usatan6012@gmail.com>
2024-08-29 15:38:52 +0900

    時間取得を世界時間から日本時間に変更

--- library/get_time.js
+++ library/get_time.js
@@ -1,6 +1,9 @@
 //時間取得
 function GetNowTime() {
-  const now = new Date();
+  const diff = 9 * 60 * 60 * 1000; // ミリ秒に変換
+  const now = new Date(new Date().getTime() + diff);//日本時間
+
+  //const now = new Date();//世界時間
   console.log(now.toISOString());
   return `${now.toISOString()}`;
 }
