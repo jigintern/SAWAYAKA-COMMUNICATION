85c287326904cd74156a1bc3b6ee72e92a0df4c0
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-09-04 21:23:31 +0900

    ✨ デイリーリザルトのCSSベース追加

--- public/daily_result/submit.js
+++ public/daily_result/submit.js
@@ -1,4 +1,4 @@
-window.onload = async (event) => {
+globalThis.onload = async (event) => {
     const current_user_profile = await JSON.parse(localStorage.getItem("current_user"))
     const id = current_user_profile.id;
 
@@ -9,11 +9,7 @@ window.onload = async (event) => {
     const data = await userResponse.json(); 
 
 
-    const usr_name_content = document.getElementById("content_container2");
-    const usr_name_el = document.getElementById("contacted_username");
-    const usercontent_el = document.getElementById("contacted_time");
-    const usercontent_time_el = document.getElementById("user_card_container_0");
-   
+    const usr_name_content = document.getElementById("itemList");   
 
 
     for (let step = 0; step < data.length; step++) {
@@ -27,17 +23,36 @@ window.onload = async (event) => {
         const current_iterate_contact_time = data[step].value.time;
         
         // 新しいdiv要素を作成
+        /**
+         * @type {HTMLDivElement}
+         */
         const outDiv = document.createElement('div');
         const idDiv = document.createElement('div');
         const timeDiv = document.createElement('div');
 
         outDiv.id = "content_container";
+        outDiv.classList.add("item")
         // コンテナに追加する
         usr_name_content.appendChild(outDiv);
-             
+
+        
+        outDiv.innerHTML = `
+        <div>
+          <div class="user_link_outer">
+            <a href="/profile?id=${current_iterate_username}">
+              <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path
+                  d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        `
+
         // 生成されたdivにidと送られたテキストを設定する
-        idDiv.textContent = `交流した相手のID:${current_iterate_username}`;
-        timeDiv.textContent = `交流した時間：${formatDateTime(current_iterate_contact_time)}`;
+        // idDiv.textContent = `交流した相手のID:${current_iterate_username}`;
+        // timeDiv.textContent = `交流した時間：${formatDateTime(current_iterate_contact_time)}`;
 
         // 生成したdivタグにidやclass名を与える
         idDiv.className = `user_card_container`;
